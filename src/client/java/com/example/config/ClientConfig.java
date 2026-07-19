dac210857e1f86f45738f44b646aad911744e70c
Alessio15102005 <alessio.decaro.it@gmail.com>
2026-07-19 20:06:59 +0200

    Aggiornato il progetto con le ultime modifiche

--- src/client/java/com/example/config/ClientConfig.java
+++ src/client/java/com/example/config/ClientConfig.java
@@ -2,7 +2,7 @@ package com.example.config;
 
 public class ClientConfig {
     public static boolean showFps = true;
-    public static boolean showCoords = false;      // Di default disattivato
-    public static boolean showDayCounter = false;  // Di default disattivato
-    public static boolean devModeActive = false;
+    public static boolean showCoords = true;      // Di default disattivato
+    public static boolean showDayCounter = true;  // Di default disattivato
+    public static boolean devModeActive = true;
 }
